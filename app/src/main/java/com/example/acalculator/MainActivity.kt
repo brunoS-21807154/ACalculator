2dddc4a835bd18366a5bc2aecd38d448db257fe2
Zushi <a21807154@alunos.ulht.pt>
2021-03-23 21:52:25 +0000

    Ficha 4 concluída

--- app/src/main/java/com/example/acalculator/MainActivity.kt
+++ app/src/main/java/com/example/acalculator/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.acalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.java.simpleName
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,55 +22,75 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        button_1.setOnClickListener {
-            Log.i(TAG, "Click no botão 1")
-            if (text_visor.text == "0") {
-                text_visor.text = "1"
-            } else {
-                text_visor.append("1")
-            }
+        button_1.setOnClickListener { onClickNumberSymbol("1"); }
+        button_2.setOnClickListener { onClickNumberSymbol("2"); }
+        button_3.setOnClickListener { onClickNumberSymbol("3"); }
+        button_4.setOnClickListener { onClickNumberSymbol("4"); }
+        button_5.setOnClickListener { onClickNumberSymbol("5"); }
+        button_6.setOnClickListener { onClickNumberSymbol("6"); }
+        button_adiction.setOnClickListener { onClickArithmeticSymbol("+"); }
+        button_subtraction.setOnClickListener { onClickArithmeticSymbol("-"); }
+        button_multiplication.setOnClickListener { onClickArithmeticSymbol("*"); }
+        button_division.setOnClickListener { onClickArithmeticSymbol("/"); }
+        button_equals.setOnClickListener { onClickEquals(); }
+        button_clear.setOnClickListener { onClickClear(); }
+        button_clear_last.setOnClickListener { onClickClearLast() }
+
+    }
+
+    private fun onClickClearLast() {
+        methodToast()
+        if(text_visor.length() > 1){
+            text_visor.text = text_visor.text.substring(0, text_visor.length() - 1)
+        }
+    }
+
+    private fun onClickClear() {
+        methodToast()
+        text_visor.text = "0"
+    }
+
+    private fun onClickNumberSymbol(symbol: String) {
+        methodToast()
+        Log.i(TAG, "Click no botão $symbol")
+        if (text_visor.text == "0") {
+            text_visor.text = symbol
+        } else {
+            text_visor.append(symbol)
+        }
+    }
+
+    private fun onClickArithmeticSymbol(symbol: String) {
+        methodToast()
+        Log.i(TAG, "Click no botão $symbol")
+        text_visor.append(symbol)
+
+    }
+
+    private fun onClickEquals() {
+        methodToast()
+        last_expression.text = text_visor.text
+
+        Log.i(TAG, "Click no botão =")
+        val expression = ExpressionBuilder(text_visor.text.toString()).build()
+
+        var result = expression.evaluate().toString()
+
+
+        if (result[result.length - 1] == '0' && result[result.length - 2] == '.') {
+            result = result.substring(0, result.length - 2)
         }
 
-        button_2.setOnClickListener {
-            Log.i(TAG, "Click no botão 2")
-            if (text_visor.text == "0") {
-                text_visor.text = "2"
-            } else {
-                text_visor.append("2")
-            }
-        }
+        text_visor.text = result
+        Log.i(TAG, "O resultado da expressão é ${text_visor.text}")
+    }
 
-        button_3.setOnClickListener {
-            Log.i(TAG, "Click no botão 3")
-            if (text_visor.text == "0") {
-                text_visor.text = "3"
-            } else {
-                text_visor.append("3")
-            }
-        }
+    private fun methodToast() {
+        val gCal = GregorianCalendar();
+        val time =
+            "${gCal.get((Calendar.HOUR_OF_DAY))}:${gCal.get((Calendar.MINUTE))}:${gCal.get((Calendar.SECOND))}"
+        val methodName = Thread.currentThread().stackTrace[3].methodName;
 
-        button_adiction.setOnClickListener{
-            Log.i(TAG, "Click no botão +")
-            text_visor.append("+")
-        }
-
-        button_equals.setOnClickListener{
-
-            last_expression.text = text_visor.text
-
-            Log.i(TAG, "Click no botão =")
-            val expression = ExpressionBuilder(text_visor.text.toString()).build()
-
-            var result = expression.evaluate().toString()
-
-
-            if(result[result.length - 1] == '0' && result[result.length - 2] == '.'){
-                result = result.substring(0, result.length - 2)
-            }
-
-            text_visor.text = result
-            Log.i(TAG, "O resultado da expressão é ${text_visor.text}")
-        }
-
+        Toast.makeText(this, "Método: $methodName, $time", Toast.LENGTH_SHORT).show()
     }
 }
